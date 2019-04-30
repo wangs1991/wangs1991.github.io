c228e1b661f9f34108bc3838dc86f98fc3f92862
王帅 <2538125068@qq.com>
2019-04-30 17:08:23 +0800

    空列表样式

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ### 目录
-+ [首页](https://wangs1991.github.io/dist/html/)
-+ [README](https://wangs1991.github.io/dist/html/readme.html)
-+ [README](https://wangs1991.github.io/dist/html/readme.html)
++ [首页](gitpage uri)
++ [常用插件工具类集合](gitpage urilibs-gather.html)
++ [README示例](gitpage urireadme.html)
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    nickname: 'wangs1991-王帅',
-    description: '前端开发工程师，总结 · 积累 · 进步',
-    title: 'wangs1991 - 不积跬步无以至千里',
-    host: 'https://wangs1991.github.io/dist/html/'
+    nickname: 'nickname',
+    description: 'one slogan',
+    title: 'page title',
+    host: 'gitpage uri'
 }
